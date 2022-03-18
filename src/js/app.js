01031237425df6304f2e438c6f44ab5a4d8c20ca
aagraston <aagraston@gmail.com>
2022-03-18 10:02:33 -0700

    state for encompassing app functional

--- src/js/app.js
+++ src/js/app.js
@@ -3,16 +3,32 @@ import React, { Component } from 'react'
 class App extends Component {
   constructor() {
     super()
+
     this.state = {
       edit: true,
     }
+  }
+
+  onSubmitForms = (e) => {
+    e.preventDefault()
+
+    this.setState({
+      edit: false,
+    })
   }
 
   render() {
     const { edit } = this.state
 
     if (edit) {
-      return <div>Currently in edit mode</div>
+      return (
+        <div>
+          <div>Currently in edit mode</div>
+          <form onSubmit={this.onSubmitForms}>
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+      )
     } else {
       return <div>Currently submitted</div>
     }
